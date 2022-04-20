f23d20c45c4713eff880f3c916e6f4e0544229f0
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-04-20 02:54:49 -0500

    Add to_datatypes for ChannelCommand

--- src/channel.rs
+++ src/channel.rs
@@ -55,3 +55,58 @@ pub enum ChannelCommand {
     Call(Call, Option<RequestId>),
     Unknown,
 }
+
+impl ChannelCommand {
+
+    fn to_datatypes(&self) -> Vec<DataType> {
+        match self {
+            ChannelCommand::Redraw {forced} => {
+                let mut args = Vec::new();
+                args.push(DataType::Boolean(*forced));
+                args
+            },
+            ChannelCommand::Normal(normal_command) => {
+                let mut args = Vec::new();
+                args.push(DataType::String(normal_command.command.clone()));
+                args
+            },
+            ChannelCommand::Call(call_command, request_id) => {
+                let mut args = Vec::new();
+                args.push(DataType::String(call_command.function.clone()));
+                args.push(DataType::List(call_command.args.clone()));
+                if let Some(request_id) = request_id {
+                    args.push(DataType::Number(request_id.0));
+                }
+                args
+            },
+            _ => unimplemented!(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_channel_command_to_datatypes() {
+
+        let expected = vec![
+            DataType::String("getline".to_string()),
+            DataType::List(vec![
+                DataType::Number(15),
+            ]),
+            DataType::Number(1),
+        ];
+        let call = ChannelCommand::Call(
+            Call {
+                function: "getline".to_string(),
+                args: vec![DataType::Number(15)],
+            },
+            Some(RequestId(1)),
+        );
+        assert_eq!(call.to_datatypes(), expected);
+
+    }
+}
