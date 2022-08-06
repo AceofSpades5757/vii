9a568ed046b7fa77b258d110ddbdb8d8f41dce5b
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-08-06 00:43:39 -0500

    add textprop module

---
+++ src/textprop.rs
@@ -0,0 +1,9 @@
+//! Text Properties
+//!
+//! Text properties are properties that can be optionally added to text to give them special meaning.
+//!
+//! The real power behind text properties is that they move with the text, regardless of position.
+//!
+//! Text properties do not have persistence outside of Vim. Once your instance closes, the text properties are removed as well.
+//!
+//! https://vimhelp.org/textprop.txt.html
