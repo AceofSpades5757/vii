181c526304f2cec31e42f1014daa3b12c64ff679
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-08-06 01:17:36 -0500

    Update

--- src/textprop.rs
+++ src/textprop.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
 //! Text Properties
 //!
 //! Text properties are properties that can be optionally added to text to give them special meaning.
@@ -7,3 +9,29 @@
 //! Text properties do not have persistence outside of Vim. Once your instance closes, the text properties are removed as well.
 //!
 //! https://vimhelp.org/textprop.txt.html
+
+use serde::{Serialize, Deserialize};
+
+/// Text Property (Base)
+#[derive(Serialize, Deserialize)]
+pub struct TextProperty {
+    id: i32,
+    r#type: String,
+}
+
+/// Text Property Type
+pub struct PropertyType {
+    highlight: Option<String>,
+    combine: Option<bool>,
+    priority: Option<i32>,
+    start_include: Option<bool>,
+    end_include: Option<bool>,
+}
+
+/// Vim Function
+pub fn prop_type_add(name: String, props: PropertyType) {
+    todo!();
+}
+
+// This represents a text property in Vim.
+// {'id': 0, 'col': 12, 'type_bufnr': 0, 'end': 1, 'type': 'number', 'length': 3, 'start': 1}
