b3a8825543d8e6f371d0945eec7d61344fa41547
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-08-07 14:03:00 -0500

    Add parse suppport for property type
    Flesh out tests

--- src/textprop.rs
+++ src/textprop.rs
@@ -14,14 +14,21 @@
 //! use vii::textprop::{PropertyTypeBuilder, prop_type_add};
 //!
 //! // Create New Property
+//!
 //! // :call prop_type_add('number', {'highlight': 'Constant'})
 //! let prop_type = PropertyTypeBuilder::default().highlight("Constant".to_string()).build();
-//! prop_type_add(String::from("number"), prop_type);
+//!
+//! //prop_type_add(String::from("number"), prop_type);
 //! // Add Property to some Text
+//!
 //! // :call prop_add('number', {'highlight': 'Constant'})
 //! ```
 
+use std::str::FromStr;
+
 use serde::{Serialize, Deserialize};
+
+use super::types::Function;
 
 type HighlightGroup = String;
 
@@ -33,13 +40,22 @@ pub struct TextProperty {
 }
 
 /// Text Property Type
-#[derive(Default)]
+#[derive(Default, Serialize, Deserialize)]
 pub struct PropertyType {
     highlight: Option<HighlightGroup>,
     combine: Option<bool>,
     priority: Option<i32>,
     start_include: Option<bool>,
     end_include: Option<bool>,
+}
+
+/// Parse JSON
+impl FromStr for PropertyType {
+    type Err = serde_json::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        serde_json::from_str(s)
+    }
 }
 
 #[derive(Default)]
@@ -50,6 +66,7 @@ pub struct PropertyTypeBuilder {
     start_include: Option<bool>,
     end_include: Option<bool>,
 }
+
 impl PropertyTypeBuilder {
     pub fn build(self) -> PropertyType {
         PropertyType {
@@ -86,7 +103,9 @@ impl PropertyTypeBuilder {
 }
 
 /// Vim Function
-pub fn prop_type_add(name: String, props: PropertyType) {
+// pub fn prop_type_add(name: String, props: PropertyType) -> super::channel::Call {
+// pub fn prop_type_add(name: String, props: PropertyType) -> super::types::Function {
+pub fn prop_type_add(name: String, props: PropertyType) -> Function {
     todo!();
 }
 
@@ -98,9 +117,38 @@ mod tests {
 
     #[test]
     fn test_property_type() {
+        let pt = PropertyType {
+            highlight: Some("Constant".to_string()),
+            combine: Some(true),
+            priority: Some(-10),
+            start_include: Some(true),
+            end_include: Some(true),
+        };
+        assert_eq!(pt.highlight, Some("Constant".to_string()));
+        assert_eq!(pt.combine, Some(true));
+        assert_eq!(pt.priority, Some(-10));
+        assert_eq!(pt.start_include, Some(true));
+        assert_eq!(pt.end_include, Some(true));
+    }
+    #[test]
+    fn test_property_type_empty() {
+        let pt: PropertyType = Default::default();
+        assert_eq!(pt.highlight, None);
+        assert_eq!(pt.combine, None);
+        assert_eq!(pt.priority, None);
+        assert_eq!(pt.start_include, None);
+        assert_eq!(pt.end_include, None);
+    }
+    #[test]
+    fn test_property_type_builder() {
         let pt = PropertyTypeBuilder::default()
             .highlight("Constant".to_string())
             .build();
         assert_eq!(pt.highlight, Some("Constant".to_string()));
+    }
+    #[test]
+    fn test_functions() {
+        let pt: PropertyType = Default::default();
+        prop_type_add(String::from("number"), pt);
     }
 }
